d9a840f404dbaa03cfc8d6e7ad9c4f5e75169166
Marco Medina <marco@wizeline.com>
2015-06-20 22:02:59 -0500

    Part 2: Inheritance

--- app.js
+++ app.js
@@ -16,6 +16,23 @@
     }
   };
 
+  var Instances = {
+    Car: function(name, seats, wheels) {
+      Abstract.Vehicle.call(this, name, seats, wheels);
+    },
+    Motorcycle: function(name, seats, wheels) {
+      Abstract.Vehicle.call(this, name, seats, wheels);
+    }
+  };
+
+  // Car inherits from Vehicle
+  Instances.Car.prototype = new Abstract.Vehicle();
+  Instances.Car.prototype.constructor = Abstract.Vehicle;
+  
+  // Motorcycle inherits from Vehicle
+  Instances.Motorcycle.prototype = new Abstract.Vehicle();
+  Instances.Motorcycle.prototype.constructor = Abstract.Vehicle;
+
   // Export to window
 	window.SC = window.SC || {};
 	window.SC.Abstract = Abstract;

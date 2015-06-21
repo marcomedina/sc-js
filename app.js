4dcfaffbf0e4d94ba8962f438234dc3706f29847
Marco Medina <marco@wizeline.com>
2015-06-20 22:43:40 -0500

    Part 4: UI

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
-(function (window) {
+(function (window, document) {
 	'use strict';
 
-  var Abstract = {
-    Vehicle: function(name, seats, wheels) {
-      this.name = name;
-      this.seats = seats;
-      this.wheels = wheels;
+  var UI = {
+    addVehicle: function(name, type) {
+      if(type==='car') {
+        var vehicle = new SC.Instances.Car(name);
+      }
+      if(type==='motorcycle') {
+        var vehicle = new SC.Instances.Motorcycle(name);
+      }
 
-      this.get = function(prop) {
-        return this[prop];
-      }
-      this.set = function(prop, value) {
-        this[prop] = value;
-      }
+      var node = document.createElement('li');
+      var textNode = document.createTextNode(vehicle.name);
+      var buttonNode = document.createElement('input');
+      buttonNode.type = "button";
+      buttonNode.value = 'Info';
+      node.appendChild(textNode);  
+      node.appendChild(buttonNode);  
+
+      document.getElementById('vehicles-list').appendChild(node);
     }
   };
-
-  var Instances = {
-    Car: function(name, seats, wheels) {
-      Abstract.Vehicle.call(this, name, seats, wheels);
-    },
-    Motorcycle: function(name, seats, wheels) {
-      Abstract.Vehicle.call(this, name, seats, wheels);
-    }
-  };
-
-  // Car inherits from Vehicle
-  Instances.Car.prototype = new Abstract.Vehicle();
-  Instances.Car.prototype.constructor = Abstract.Vehicle;
-  
-  // Motorcycle inherits from Vehicle
-  Instances.Motorcycle.prototype = new Abstract.Vehicle();
-  Instances.Motorcycle.prototype.constructor = Abstract.Vehicle;
 
   // Export to window
 	window.SC = window.SC || {};
-	window.SC.Abstract = Abstract;
-})(window)
+	window.SC.UI = UI;
+})(window, document)
+
---
+++ classes.js
@@ -0,0 +1,44 @@
+(function (window) {
+	'use strict';
+
+  var Abstract = {
+    Vehicle: function(name, seats, wheels) {
+      this.name = name;
+      this.seats = seats || 0;
+      this.wheels = wheels || 0;
+
+      this.get = function(prop) {
+        return this[prop];
+      }
+      this.set = function(prop, value) {
+        this[prop] = value;
+      }
+
+      this.info = function() {
+        alert('Name: ' + this.name + ', Seats: ' + this.seats + ', Wheels: ' + this.wheels);
+      }
+    }
+  };
+
+  var Instances = {
+    Car: function(name, seats, wheels) {
+      Abstract.Vehicle.call(this, name, seats, wheels);
+    },
+    Motorcycle: function(name, seats, wheels) {
+      Abstract.Vehicle.call(this, name, seats, wheels);
+    }
+  };
+
+  // Car inherits from Vehicle
+  Instances.Car.prototype = new Abstract.Vehicle();
+  Instances.Car.prototype.constructor = Abstract.Vehicle;
+  
+  // Motorcycle inherits from Vehicle
+  Instances.Motorcycle.prototype = new Abstract.Vehicle();
+  Instances.Motorcycle.prototype.constructor = Abstract.Vehicle;
+
+  // Export to window
+	window.SC = window.SC || {};
+	window.SC.Abstract = Abstract;
+	window.SC.Instances = Instances;
+})(window)
